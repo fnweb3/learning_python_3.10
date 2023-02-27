75e9fb0497316a2541d2fdf369ff6d333851cb6a
Fernando Noguera <66801905+fnweb3@users.noreply.github.com>
2023-02-27 18:15:18 -0300

    Ejercicios_Semana_01

---
+++ Ejercicio1.py
@@ -0,0 +1,21 @@
+'''
+a. Crea 3 variables, una entera, una flotante y una cadena de texto.
+b. Imprime cada una de las variables en la consola.
+c. Crea una variable booleana y asignale un valor.
+d. Imprime la variable booleana en la consola.
+
+'''
+
+# Declaro las tres variables, con sus respectivos tipos aclarados en sus nombres
+
+variable_entera = 75
+variable_flotante = 15.25
+variable_cadena = "Hola Mundo"
+variable_booleana = True
+
+# completa los valores de las variables de acuerdo a su tipo
+# ... (ahora podes seguir con el código....)
+print ("Valor de la variable entera = ", variable_entera)
+print ("Valor de la variable flotante = ", variable_flotante)
+print ("Valor de la variable cadena = ", variable_cadena)
+print ("Valor de la variable booleana = ", variable_booleana)
